efd7c4bc4edd4d838770a0499ac7bf930e8f348d
shamimhossain515419 <=>
2025-07-04 10:47:45 +0600

    Add product model and CRUD

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from .views import hello_world
+
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet
+
+router = DefaultRouter()
+router.register(r"products", ProductViewSet)
 
 urlpatterns = [
     path("hello/", hello_world),
+    path("", include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,16 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from api.models import Product
+from api.serializers import ProductSerializer
+
+from rest_framework import viewsets
 
 
 @api_view(["GET"])
 def hello_world(request):
     return Response({"message": "Hello, World!"})
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
